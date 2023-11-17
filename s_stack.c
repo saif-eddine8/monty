36de4fbd698dcda6ebda7b8d921f55db5d6ff48a
AYOUB-ash <elasaadayoub.contact@gmail.com>
2023-11-17 01:20:08 -0800

    Stacks, Queues

--- s_stack.c
+++ s_stack.c
@@ -1,9 +1,9 @@
 #include "monty.h"
 
 /**
- * char_print - Prints the Ascii value.
- * @stack: Pointer to a pointer pointing to top node of the stack.
- * @line_number: Interger representing the line number of of the opcode.
+ * char_print - that Prints the Ascii value.
+ * @stack:is the Pointer to a pointer pointing to top node of the stack.
+ * @line_number: is the Interger representing the line number of of the opcode.
  */
 void char_print(stack_t **stack, unsigned int line_number)
 {
@@ -18,62 +18,11 @@ void char_print(stack_t **stack, unsigned int line_number)
 	printf("%c\n", ascii);
 }
 
-/**
- * str_print - Prints a string.
- * @stack: Pointer to a pointer pointing to top node of the stack.
- * @ln: Interger representing the line number of of the opcode.
- */
-void str_print(stack_t **stack, __attribute__((unused))unsigned int ln)
-{
-	int ascii;
-	stack_t *tmp;
-
-	if (stack == NULL || *stack == NULL)
-	{
-		printf("\n");
-		return;
-	}
-
-	tmp = *stack;
-	while (tmp != NULL)
-	{
-		ascii = tmp->n;
-		if (ascii <= 0 || ascii > 127)
-			break;
-		printf("%c", ascii);
-		tmp = tmp->next;
-	}
-	printf("\n");
-}
 
 /**
- * rot_f - Rotates the first node of the stack to the bottom.
- * @stack: Pointer to a pointer pointing to top node of the stack.
- * @ln: Interger representing the line number of of the opcode.
- */
-void rot_f(stack_t **stack, __attribute__((unused))unsigned int ln)
-{
-	stack_t *tmp;
-
-	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
-		return;
-
-	tmp = *stack;
-	while (tmp->next != NULL)
-		tmp = tmp->next;
-
-	tmp->next = *stack;
-	(*stack)->prev = tmp;
-	*stack = (*stack)->next;
-	(*stack)->prev->next = NULL;
-	(*stack)->prev = NULL;
-}
-
-
-/**
- * rot_l - Rotates the last node of the stack to the top.
- * @stack: Pointer to a pointer pointing to top node of the stack.
- * @ln: Interger representing the line number of of the opcode.
+ * rot_l - functin that Rotates the last node of the stack to the top.
+ * @stack: is Pointer to a pointer pointing to top node of the stack.
+ * @ln: is the Interger representing the line number of of the opcode.
  */
 void rot_l(stack_t **stack, __attribute__((unused))unsigned int ln)
 {
@@ -92,4 +41,54 @@ void rot_l(stack_t **stack, __attribute__((unused))unsigned int ln)
 	tmp->prev = NULL;
 	(*stack)->prev = tmp;
 	(*stack) = tmp;
+}
+/**
+ * rot_f - is the Rotates the first node of the stack to the bottom.
+ * @stack: the Pointer to a pointer pointing to top node of the stack.
+ * @ln: the Interger representing the line number of of the opcode.
+ */
+void rot_f(stack_t **stack, __attribute__((unused))unsigned int ln)
+{
+        stack_t *tmp;
+
+        if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+                return;
+
+        tmp = *stack;
+        while (tmp->next != NULL)
+                tmp = tmp->next;
+
+        tmp->next = *stack;
+        (*stack)->prev = tmp;
+        *stack = (*stack)->next;
+        (*stack)->prev->next = NULL;
+        (*stack)->prev = NULL;
+}
+
+/**
+ * str_print - function that Prints a string.
+ * @stack: is the Pointer to a pointer pointing to top node of the stack.
+ * @ln: the Interger representing the line number of of the opcode.
+ */
+void str_print(stack_t **stack, __attribute__((unused))unsigned int ln)
+{
+        int ascii;
+        stack_t *tmp;
+
+        if (stack == NULL || *stack == NULL)
+        {
+                printf("\n");
+                return;
+        }
+
+        tmp = *stack;
+        while (tmp != NULL)
+        {
+                ascii = tmp->n;
+                if (ascii <= 0 || ascii > 127)
+                        break;
+                printf("%c", ascii);
+                tmp = tmp->next;
+        }
+        printf("\n");
 }
